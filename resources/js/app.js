edbba64986bf57b3e441bc96094a2b01d6dfe56a
Song <nosongyang@gmail.com>
2014-05-20 17:21:18 +0800

    intro

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ var app = angular.module('Dream',['ngRoute']);
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider.otherwise({redirectTo:'/'});
     $routeProvider.when('/',{templateUrl:'resources/html/welcome.html'});
-    $routeProvider.when('/intro',{templateUrl:'resources/html/project_intro.html'});
+    $routeProvider.when('/intro',{controller:'projectController',templateUrl:'resources/html/project_intro.html'});
     $routeProvider.when('/project',{templateUrl:'resources/html/project_con.html'});
     $routeProvider.when('/artist',{templateUrl:'resources/html/people.html'});
     $routeProvider.when('/building',{templateUrl:'resources/html/building.html'});
---
+++ resources/js/controllers/project_controller.js
@@ -0,0 +1,5 @@
+app.controller('projectController',function($scope,$location) {
+    $scope.textTitle = "ssssssssssssss";
+    $scope.textBody = "sadfasdfas";
+    $scope.imageName = "booking1.png"
+});
