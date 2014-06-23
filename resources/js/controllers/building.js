95ee8cf9a846325568363d188bfb7677ce550caa
Song <nosongyang@gmail.com>
2014-06-23 11:42:09 +0800

    new

--- resources/js/controllers/building.js
+++ resources/js/controllers/building.js
@@ -5,34 +5,50 @@ app.controller('buildingController',function($scope,$location) {
         {
             case 1:
                 $scope.buildingImgName = "building1.png";
+                doSomeAnimation();
                 break;
             case 2:
                 $scope.buildingImgName = "1-1.png";
+                doSomeAnimation();
                 break;
             case 3:
                 $scope.buildingImgName = "2-1.png";
+                doSomeAnimation();
                 break;
             case 4:
                 $scope.buildingImgName = "3-1.png";
+                doSomeAnimation();
                 break;
             case 5:
                 $scope.buildingImgName = "1-1.png";
+                doSomeAnimation();
                 break;
             case 6:
                 $scope.buildingImgName = "2-1.png";
+                doSomeAnimation();
                 break;
             case 7:
                 $scope.buildingImgName = "3-1.png";
+                doSomeAnimation();
                 break;
             case 8:
                 $scope.buildingImgName = "4-1.png";
+                doSomeAnimation();
                 break;
             case 9:
                 $scope.buildingImgName = "building1.png";
+                doSomeAnimation();
                 break;
             case 10:
                 $scope.buildingImgName = "1-1.png";
+                doSomeAnimation();
                 break;
         }
     };
+    function doSomeAnimation(){
+        $('#imageShow').addClass('animated' + ' fadeInDown').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            $(this).removeClass();
+
+        });       
+    }
 });
